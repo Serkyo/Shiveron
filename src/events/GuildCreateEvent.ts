b7a6709a33339fde7b951afa82f4fd480fbb7cba
Serkyo <serkyo-dev@proton.me>
2025-11-08 19:45:25 +0100

    Added guild registration on join and leave to clear db

---
+++ src/events/GuildCreateEvent.ts
@@ -0,0 +1,19 @@
+import { Guild } from 'discord.js';
+import { BaseEvent } from '../core/BaseEvent.js';
+import { ShiveronClient } from '../core/ShiveronClient.js';
+import { GuildSettingsService } from '../services/GuildSettingsService.js';
+import { ShiveronLogger } from '../utils/ShiveronLogger.js';
+
+export default class GuildCreateEvent extends BaseEvent<'guildCreate'> {
+	public readonly name = 'guildCreate';
+	public once = false;
+
+	public async execute(_client: ShiveronClient, guild: Guild): Promise<void> {
+		try {
+			await GuildSettingsService.createOrGetGuildSettings(guild.id);
+		}
+		catch (error) {
+			ShiveronLogger.error(`Failed to process guild join : ${error}`);
+		}
+	}
+}
---
+++ src/events/GuildDeleteEvent.ts
@@ -0,0 +1,19 @@
+import { Guild } from 'discord.js';
+import { BaseEvent } from '../core/BaseEvent.js';
+import { ShiveronClient } from '../core/ShiveronClient.js';
+import { GuildSettingsService } from '../services/GuildSettingsService.js';
+import { ShiveronLogger } from '../utils/ShiveronLogger.js';
+
+export default class GuildDeleteEvent extends BaseEvent<'guildDelete'> {
+	public readonly name = 'guildDelete';
+	public once = false;
+
+	public async execute(_client: ShiveronClient, guild: Guild): Promise<void> {
+		try {
+			await GuildSettingsService.deleteGuildSettings(guild.id);
+		}
+		catch (error) {
+			ShiveronLogger.error(`Failed to process guild leave : ${error}`);
+		}
+	}
+}
